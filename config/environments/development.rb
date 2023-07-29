ef6ca1412556e654a4428e475174c9cfa23f522e
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-29 11:35:34 -0300

    FEAT:Instalando y configurando devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -67,4 +68,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
