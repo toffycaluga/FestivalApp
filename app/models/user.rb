3614b6f29588f768b08179ed3f7870c63091d792
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-29 11:40:58 -0300

    FEAT: Creando usuarios con devise

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  name                   :string
+#  phone_number           :string
+#  role                   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  #CanCanCan
+  ROLES = %w[Admin Jurado Usuario Organizador].freeze
+
+  #Validaciones 
+  validates :name, presence: true
+  validates :phone_number, presence: true
+end
