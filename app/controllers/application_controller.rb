bd0142caac975c1d68cfe1a8f29bcc8f660667ed
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-30 12:19:48 -0300

    FEAT: Imagenes enlazadas a usuario , falta convertir la imagen a un thumbs para utilizarla

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_photo, :phone_number])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_photo, :phone_number])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :phone_number, :resize_avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :phone_number, :resize_avatar])
     end
 end
