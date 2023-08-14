4bf2ac4ce42267809e965aa54700342b3af631c7
Toffy Caluga <abraham.lillol@gmail.com>
2023-08-14 12:28:27 -0300

    FEAT: Cierre de postulaciones listos y Readme creado

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -58,6 +58,17 @@ class FestivalsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  def close_applications
+    @festival = Festival.find(params[:id])
+    @festival.update(application_state: false)
+    redirect_to admin_festival_path(@festival), notice: "Postulaciones cerradas"
+  end
+
+  def close_festival
+      @festival = Festival.find(params[:id])
+      @festival.update(state: false)
+      redirect_to admin_festival_path(@festival), notice: "Festival cerrado"
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     get 'dashboard', to: 'dashboard#index'
+    resources :festivals do
+      put :close_applications
+      put :close_festival
+    end
 
   end
 
